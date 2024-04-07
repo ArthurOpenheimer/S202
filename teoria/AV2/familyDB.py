b699a02e53b26ab0b938a2e7b113e3b664daabd1
Arthur <artopenheimervilela@gmail.com>
2024-04-07 17:42:29 -0300

    style: remover comentarios desnecessarios

--- teoria/AV2/familyDB.py
+++ teoria/AV2/familyDB.py
@@ -14,8 +14,6 @@ class FamilyDB:
         return [result['p']['nome'] for result in results]
 
     def get_relationship(self, nome):
-        #busca: Luana namora com quem desde quando?
-        #fazer o match retornar o nome e a data de inicio do relacionamento (propriedade do relacionamento)
         query = f"MATCH (p:Pessoa {{nome: '{nome}'}})-[r:NAMORADO_DE]->(c:Pessoa) RETURN c, r"
         results = self.db.execute_query(query)
         return [(result['c']['nome'], result['r']['desde']) for result in results]
--- teoria/AV2/main.py
+++ teoria/AV2/main.py
@@ -1,13 +1,9 @@
 from familyDB import FamilyDB
 from database import Database
 
-# cria uma instância da classe Database, passando os dados de conexão com o banco de dados Neo4j
-#family_db = FamilyDB("bolt://44.195.93.13:7687", "neo4j", "diagnoses-silks-characteristic")
-
 db = Database("bolt://44.195.93.13:7687", "neo4j", "diagnoses-silks-characteristic")
 family_db = FamilyDB(db)
 
-# Imprimindo todas as informações do banco de dados
 opcao = int(input("""[1] - Quem são os pais de Lucas?
 [2] - Quem é irmão da Luana?
 [3] - Luana namora com quem desde quando?
@@ -22,5 +18,4 @@ elif opcao == 3:
     print("Namorado de Luana e data de início do relacionamento:")
     print(family_db.get_relationship("Luana"))
 
-# Fechando a conexão com o banco de dados
 db.close()
